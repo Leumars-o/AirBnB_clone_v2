123942c13de0ff24641cb93954eb86eec129b179
leumars-o <samex150@gmail.com>
2024-05-07 05:29:17 +0100

    deploy archive script modified

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,6 +7,7 @@ from fabric.api import put, run, env
 from os.path import exists
 env.hosts = ['54.157.155.48', '100.26.171.101']
 
+
 def do_deploy(archive_path):
     """Distributes an archive to your web servers
     """
@@ -17,12 +18,16 @@ def do_deploy(archive_path):
         filename = archive_path.split("/")[-1]
         foldername = filename.split(".")[0]
         run("mkdir -p /data/web_static/releases/{}".format(foldername))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(filename, foldername))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format
+            (filename, foldername))
         run("rm /tmp/{}".format(filename))
-        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(foldername, foldername))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(foldername))
+        run("mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/".format(foldername, foldername))
+        run("rm -rf /data/web_static/releases/{}/web_static"
+            .format(foldername))
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{} /data/web_static/current".format(foldername))
+        run("ln -s /data/web_static/releases/{} /data/web_static/current"
+            .format(foldername))
         return True
-    except:
+    except Exception as e:
         return False
